a0b09d4aba611aa1b34e8fe9b65ac002784e9d8e
Alexandr <green_83@list.ru>
2021-12-16 11:27:11 +0300

    workflow-deploy-4

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! С третьей попытки!')
+    return HttpResponse('У меня получилось! С четвёртой попытки!')
 
 
 def second_page(request):
